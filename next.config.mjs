a0cbff9135063e518968695427d22e2e6f553dbe
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-09-28 22:41:13 +0300

    Update of eslint config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = process.env.DEV ? {} :
+    {
+        output: 'export',
+        distDir: 'docs',
+        basePath: '/multikids',
+    };
 
 export default nextConfig;
