cb3e66d0c52736ef21f42101c8249a7d166b97bb
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-10-12 11:46:55 +0300

    Main game keyboard events

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 import {ActionCode, Keyboard} from "@/components/Keyboard";
 import {Game, NewEquation} from "@/components/Game";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {Score} from "@/components/Score";
 import {SFX} from "@/components/SFX";
 import {GameModel} from "@/models/GameModel";
@@ -8,15 +8,25 @@ import {Button} from "@/components/Button";
 import {ButtonMute} from "@/components/ButtonMute";
 import {Help} from "@/components/Help";
 
+const NumberKeys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].reduce((a, t) => [...a, `Digit${t}`, `Numpad${t}`], [] as string[]);
+
 export function Main({basePath, gameModel}: {
     basePath: string,
     gameModel: GameModel,
 }) {
 
+    const main = useRef<HTMLElement>(null);
     const [newEquation, setNewEquation] = useState<NewEquation>(false);
     const [input, setInput] = useState(-1);
     const [helpOpen, setHelpOpen] = useState(false);
     const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener("keydown", onKeyDown, false);
+        return () => {
+            document.removeEventListener("keydown", onKeyDown, false);
+        }
+    }, [input]);
 
     useEffect(() => {
         if (!init) {
@@ -53,28 +63,48 @@ export function Main({basePath, gameModel}: {
         }
     }
 
-    return <main>
+    function onKeyDown(event: KeyboardEvent) {
+        if (event.key === "Escape") {
+            main.current?.focus();
+        }
+        if (main.current !== document.activeElement && main.current?.contains(document.activeElement)) {
+            return;
+        }
+        if (NumberKeys.indexOf(event.code) >= 0) {
+            onInput(parseInt(event.code.replace(/\D+/ig, "")));
+        } else if (event.code === "Backspace" || event.code === "Delete") {
+            onInput(ActionCode.Delete);
+        } else if (event.code === "Enter" || event.code === "NumpadEnter" || event.code === "Space") {
+            onInput(ActionCode.Enter);
+        }
+    }
+
+    function onClickInventoryItem(item: string) {
+        if (gameModel.isUnlocked(item)) {
+            gameModel.setBg(item);
+            return true;
+        } else if (gameModel.purchase(item)) {
+            gameModel.setBg(item);
+            return true;
+        }
+        return false;
+    }
+
+    function onCloseHelpModal() {
+        setHelpOpen(false);
+        setInput(-1);
+        setNewEquation("replace");
+    }
+
+    return <main ref={main} tabIndex={-1}>
         <Game newEquation={newEquation} setNewEquation={setNewEquation} input={input} gameModel={gameModel}/>
         <Keyboard onClick={onInput}/>
-        {init && <Score gameModel={gameModel} onClickItem={item => {
-            if (gameModel.isUnlocked(item)) {
-                gameModel.setBg(item);
-                return true;
-            } else if (gameModel.purchase(item)) {
-                gameModel.setBg(item);
-                return true;
-            }
-            return false;
-        }}/>}
+        {init && <Score gameModel={gameModel} onClickItem={onClickInventoryItem}/>}
         {init && <div className="controls">
             <Button className="button-help" onClick={() => setHelpOpen(true)}>?</Button>
             <ButtonMute selected={gameModel.getMute()}
                         setSelected={value => gameModel.setMute(value)}/>
         </div>}
-        <Help open={helpOpen} onClose={() => {
-            setHelpOpen(false);
-            setInput(-1);
-            setNewEquation("replace");
-        }}/>
+        <Help open={helpOpen} onClose={onCloseHelpModal}/>
     </main>;
 }
