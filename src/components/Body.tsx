72f47eec9e228785b2ebc45c69671a43b430219d
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2024-10-14 23:52:38 +0300

    Added VK Bridge

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,11 +3,16 @@
 import {Background} from "@/components/Background";
 import {Main} from "@/components/Main";
 import {useGameModel} from "@/models/GameModel";
+import {useState} from "react";
+import bridge from "@vkontakte/vk-bridge";
 
 export function Body({basePath}: {
     basePath: string
 }) {
 
+    useState(() => {
+        bridge.send("VKWebAppInit", {});
+    });
     const gameModel = useGameModel();
 
     return <>
