1a7de6159f61f64d8188b7f19fcebd7821f2bed7
Evgeniy Polyakov <evgeniy.s.polyakov@gmail.com>
2025-01-02 17:03:19 +0300

    4.0.0

--- docs/_next/static/chunks/app/layout-ddb2faa66691e487.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7288:function(n,e,u){Promise.resolve().then(u.t.bind(u,4582,23))},4582:function(){}},function(n){n.O(0,[431,971,23,744],function(){return n(n.s=7288)}),_N_E=n.O()}]);
